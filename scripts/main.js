c1c1a98494273824bcde536a850cd8d0a6e50b40
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-08 15:29:37 -0600

    Setup fetch and DOM injection of random fox images

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+fetch('https://randomfox.ca/floof')
+    .then(response => response.json())
+    .then(
+        (convertedData) => {
+            const foxImgElement = document.querySelector("#fox")
+            foxImgElement.src = convertedData.image
+        }
+    )
