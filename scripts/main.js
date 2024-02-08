fdd1154e72d4817b413d8fd05a4121cf7c33d8ba
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-08 15:44:20 -0600

    Setup fetch and DOM injection of random cat image

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,30 @@
-fetch('https://randomfox.ca/floof')
+    const getTheFox = async () => {
+        // Fetch a random fox image
+        const response = await fetch('https://randomfox.ca/floof')
+        const convertedData = await response.json()
+
+        // Inject random fox image into the DOM
+        const foxImgElement = document.querySelector("#fox")
+        foxImgElement.src = convertedData.image
+    }
+
+    const getTheCat = async () => {
+        // Fetch a random cat image
+        const response = await fetch('http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false')
+        const convertedData = await response.json()
+        
+        // Inject random cat image into the DOM
+        const catImgElement = document.querySelector("#cat")
+        catImgElement.src = convertedData[0]
+    }
+
+
+    getTheFox()
+    getTheCat()
+
+
+/*
+    fetch('https://randomfox.ca/floof')
     .then(response => response.json())
     .then(
         (convertedData) => {
@@ -6,3 +32,13 @@ fetch('https://randomfox.ca/floof')
             foxImgElement.src = convertedData.image
         }
     )
+
+    fetch('http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false')
+        .then(response => response.json())
+        .then(
+            (convertedData) => {
+                const catImgElement = document.querySelector("#cat")
+                catImgElement.src = convertedData[0]
+            }
+        )
+*/
